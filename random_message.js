cb01f21b2a3b6d875bdfdbab8274bf99afe80e38
Ruben A Gonzalez <ruby@Rubens-MacBook-Air.local>
2021-02-09 13:36:14 -0800

    Start of project

---
+++ random_message.js
@@ -0,0 +1,19 @@
+// Initiate message container
+const messageContainer = {
+    quotes: [],
+    teams: [],
+    
+    // Get a random quote
+    getRandomQuote() {
+
+    },
+
+    // Get a random team
+    getRandomTeam() {
+
+    }
+};
+
+
+// Format message
+console.log('Start of script');
